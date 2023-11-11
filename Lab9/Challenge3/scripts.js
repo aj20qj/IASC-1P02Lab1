066f405c4992aa3ff1e8860223b67af2141b891e
aj20qj <144570336+aj20qj@users.noreply.github.com>
2023-11-11 18:14:25 -0500

    Update

--- Lab9/Challenge3/scripts.js
+++ Lab9/Challenge3/scripts.js
@@ -2,5 +2,5 @@ function sumButton(){
   var numb1 = document.getElementById("num1").value;
   var numb2 = document.getElementById("num2").value;
   var sum = Number(numb1) + Number(numb2);
-  document.getElementById("resultID").value = sum;
+  document.getElementById("resultID").innerHTML = sum;
 }
